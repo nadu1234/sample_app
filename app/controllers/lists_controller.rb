edc5996801b52601074a7815a519e5bd65586b12
nadu1234 <146610305+nadu1234@users.noreply.github.com>
2023-10-08 06:55:05 +0000

    {Finsh}3-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,11 +6,17 @@ class ListsController < ApplicationController
 
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
+    # list = List.new(list_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
+    # list.save
     # 4. トップ画面へリダイレクト
-    redirect_to list_path(list.id)
+    # redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
   def index
